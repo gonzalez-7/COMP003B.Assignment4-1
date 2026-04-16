b5adbd34c3ed7073f9c3d81ae84b1f634899ecce
gonzalez-7 <20agonzalezp@gmail.com>
2026-04-15 17:44:13 -0700

    Created 2 models classes for Trail and Hike Registration

---
+++ COMP003B.Assignment4/Models/HikeRegistration.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B.Assignment4.Models
+{
+	public class HikeRegistration
+	{
+		public int Id { get; set; }
+
+		[Required]
+		[StringLength(50)]
+		public string FullName { get; set; } = string.Empty;
+
+		[Required]
+		[EmailAddress]
+		public string Email { get; set; } = string.Empty;
+
+		[Range(1, 20)]
+		public int GroupSize { get; set; }
+
+		[Required]
+		public int TrailId { get; set; }
+
+		[StringLength(100)]
+		public string Notes { get; set; } = string.Empty;
+	}
+}
---
+++ COMP003B.Assignment4/Models/Trail.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B.Assignment4.Models
+{
+	public class Trail
+	{
+		public int Id { get; set; }
+
+		[Required]
+		[StringLength(50)]
+		public string Name { get; set; } = string.Empty;
+
+		[Required]
+		[StringLength(100)]
+		public string Location { get; set; } = string.Empty;
+
+		[Range(1, 30)]
+		public double DistanceMiles { get; set; }
+
+		[Required]
+		[StringLength(20)]
+		public string Difficulty { get; set; } = string.Empty;
+	}
+}
